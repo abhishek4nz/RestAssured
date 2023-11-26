252409474a16507f63857f4b8002ac6a17415561
abhishek4nz <abhishek4nz@gmail.com>
2023-11-26 19:44:05 +0530

    My first API test on Google Place ADD API

---
+++ src/main/java/org/RestAssured/Basics.java
@@ -0,0 +1,38 @@
+package org.RestAssured;
+
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
+
+public class Basics {
+
+    //VALIDATE IF THE ADD API IS WORKING AS EXPECTED
+
+    public static void main(String[] args) {
+
+        RestAssured.baseURI = "https://rahulshettyacademy.com/";        //SET BASE URL
+
+        //given - all input fields
+        //when - submit the api (Resource, HTTP methods)
+        //then - validate the response
+
+        given().log().all().queryParam("key","qaclick123").header("Content-Type","application/json")
+                .body("{\n" +
+                        "    \"location\": {\n" +
+                        "        \"lat\": -38.383494,\n" +
+                        "        \"lng\": 33.427362\n" +
+                        "    },\n" +
+                        "    \"accuracy\": 50,\n" +
+                        "    \"name\": \"Frontline house\",\n" +
+                        "    \"phone_number\": \"(+91) 983 893 3937\",\n" +
+                        "    \"address\": \"29, side layout, cohen 09\",\n" +
+                        "    \"types\": [\n" +
+                        "        \"shoe park\",\n" +
+                        "        \"shop\"\n" +
+                        "    ],\n" +
+                        "    \"website\": \"http://google.com\",\n" +
+                        "    \"language\": \"French-IN\"\n" +
+                        "}").when().post("/maps/api/place/add/json")        //SUBMIT HTTP REQUEST
+                .then().log().all().assertThat().statusCode(200);       //VALIDATE THAT STATUS CODE IS 200
+
+    }
+}
