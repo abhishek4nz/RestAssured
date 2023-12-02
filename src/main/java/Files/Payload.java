b206e02746704985630332ceddfe090f8b163d5e
abhishek4nz <abhishek4nz@gmail.com>
2023-12-02 21:01:30 +0530

    Now I am parsing JSON values in string variables

--- src/main/java/Files/Payload.java
+++ src/main/java/Files/Payload.java
@@ -3,21 +3,21 @@ package Files;
 public class Payload {
 
     public static String add() {
-        return "{\\n\" +\n" +
-                "                        \"    \\\"location\\\": {\\n\" +\n" +
-                "                        \"        \\\"lat\\\": -38.383494,\\n\" +\n" +
-                "                        \"        \\\"lng\\\": 33.427362\\n\" +\n" +
-                "                        \"    },\\n\" +\n" +
-                "                        \"    \\\"accuracy\\\": 50,\\n\" +\n" +
-                "                        \"    \\\"name\\\": \\\"Frontline house\\\",\\n\" +\n" +
-                "                        \"    \\\"phone_number\\\": \\\"(+91) 983 893 3937\\\",\\n\" +\n" +
-                "                        \"    \\\"address\\\": \\\"29, side layout, cohen 09\\\",\\n\" +\n" +
-                "                        \"    \\\"types\\\": [\\n\" +\n" +
-                "                        \"        \\\"shoe park\\\",\\n\" +\n" +
-                "                        \"        \\\"shop\\\"\\n\" +\n" +
-                "                        \"    ],\\n\" +\n" +
-                "                        \"    \\\"website\\\": \\\"http://google.com\\\",\\n\" +\n" +
-                "                        \"    \\\"language\\\": \\\"French-IN\\\"\\n\" +\n" +
-                "                        \"}";
+        return "{\n" +
+                "    \"location\": {\n" +
+                "        \"lat\": -38.383494,\n" +
+                "        \"lng\": 33.427362\n" +
+                "    },\n" +
+                "    \"accuracy\": 50,\n" +
+                "    \"name\": \"Frontline house\",\n" +
+                "    \"phone_number\": \"(+91) 983 893 3937\",\n" +
+                "    \"address\": \"29, side layout, cohen 09\",\n" +
+                "    \"types\": [\n" +
+                "        \"shoe park\",\n" +
+                "        \"shop\"\n" +
+                "    ],\n" +
+                "    \"website\": \"http://google.com\",\n" +
+                "    \"language\": \"French-IN\"\n" +
+                "}";
     }
 }
--- src/main/java/org/RestAssured/Basics2.java
+++ src/main/java/org/RestAssured/Basics2.java
@@ -1,9 +1,11 @@
 package org.RestAssured;
 
-import Files.Payload;
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 
+import static Files.Payload.add;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class Basics2 {
     public static void main(String[] args) {
@@ -14,9 +16,26 @@ public class Basics2 {
         //when - submit the api (Resource, HTTP methods)
         //then - validate the response
 
-        given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
-                .body(Payload.add()).when().post("/maps/api/place/add/json")        //SUBMIT HTTP REQUEST
-                .then().log().all().assertThat().statusCode(200);       //VALIDATE THAT STATUS CODE IS 200
+       String response = given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
+                .body(add())                //ADD PAYLOAD FROM A SEPARATE CLASS
+                .when().post("/maps/api/place/add/json")        //SUBMIT HTTP REQUEST
+                .then().log().all().assertThat().statusCode(200)       //VALIDATE THAT STATUS CODE IS 200
+                .body("scope", equalTo("APP"))              //MATCHING JSON RESPONSE WITH HAMCREST MATCHERS
+                .header("Server","Apache/2.4.52 (Ubuntu)")     //MATCHING HEADER IN RESPONSE
+                .extract().response().asString();               //EXTRACT JSON RESPONSE AND STORE IT IN A STRING
+
+        System.out.println(response);
+
+        JsonPath js = new JsonPath(response);       //FOR PARSING JSON STORED AS A STRING
+        String placeId = js.getString("place_id");      //EXTRACTING PLACE ID AND STORING IT IN A STRING
+        System.out.println(placeId);        //PRINTING THE PLACE ID
+
+        //ADD PLACE
+        //UPDATE PLACE WITH NEW ADDRESS
+        //GET PLACE TO VALIDATE IF NEW ADDRESS IS PRESENT OR NOT
+
+
+
 
     }
 }
